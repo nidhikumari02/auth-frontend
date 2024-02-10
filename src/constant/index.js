4604213709f17f873bece30f8b43710bcf7daa3a
Nidhi kumari <122108296+nidhikumari02@users.noreply.github.com>
2024-02-10 13:35:38 +0530

    Update index.js

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:4000";
+export const baseUrl = "https://auth-backend-k96x.onrender.com";
 export const loginUrl = "/api/auth/login";
 export const registerUrl = "/api/auth/register";
 export const profileUrl = "/api/user/profile";
